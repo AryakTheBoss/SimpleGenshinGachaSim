32d14be433d8c88e8f1df32c387d269bbf0ba05b
Aryak <pandeary12@gmail.com>
2020-11-10 18:33:25 -0500

    Added Childe banner skeleton and Weapon Banner skeleton

---
+++ src/com/javamaster/ChildeBanner.java
@@ -0,0 +1,41 @@
+package com.javamaster;
+
+public class ChildeBanner implements Banner {
+
+
+
+    @Override
+    public String pull() {
+        return null;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setPities(int four, int five) {
+
+    }
+
+    @Override
+    public void printPity() {
+
+    }
+
+    @Override
+    public void printStats() {
+
+    }
+
+    @Override
+    public void copyCount(Counter ic) {
+
+    }
+
+    @Override
+    public Counter getItemCounter() {
+        return null;
+    }
+}
